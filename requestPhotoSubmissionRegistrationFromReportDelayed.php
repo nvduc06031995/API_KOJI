26a4bf70cad4c0f72e44e50cb7994ef911dc6a27
HoNgocNguyenSBA <hnnguyen@starboard-asia.com>
2022-12-01 13:52:29 +0700

    fix apt

---
+++ requestPhotoSubmissionRegistrationFromReportDelayed.php
@@ -0,0 +1,5 @@
+<?php
+
+include('resources.php');
+$postPhotoSubmissionRegistrationFromReportDelayed = new resources();
+$postPhotoSubmissionRegistrationFromReportDelayed->postPhotoSubmissionRegistrationFromReportDelayed();

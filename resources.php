dc0fa7a419bcf312027ed31643d2eeedc5877aee
nvduc06031995 <nvduc@starboardasia.com>
2022-12-02 17:32:24 +0700

    commit

--- resources.php
+++ resources.php
@@ -173,6 +173,7 @@ class resources
                 $sql = 'SELECT COUNT(*) 
                     FROM T_KOJI 
                     WHERE JYUCYU_ID="' . $JYUCYU_ID . '"
+                    AND KOJI_YMD="' . $YMD . '"
                     AND KOJI_ST="02"
                     AND DEL_FLG= 0 ';
                 $this->result = $this->dbConnect->query($sql);
@@ -183,7 +184,7 @@ class resources
                         $count_jyucyu = $row["COUNT(*)"];
                     }
                 }
-
+                
                 if ($count_jyucyu > 0) {
                     $sql = 'SELECT COUNT(*) 
                     FROM T_KOJI 
@@ -344,6 +345,7 @@ class resources
                         T_KOJI_FILEPATH.FILEPATH_ID
                         FROM T_KOJI LEFT JOIN T_KOJI_FILEPATH ON T_KOJI.JYUCYU_ID=T_KOJI_FILEPATH.ID 
                         WHERE SYUYAKU_JYUCYU_ID= "' . $JYUCYU_ID . '"
+                        AND (T_KOJI_FILEPATH.FILE_KBN_CD="03" OR T_KOJI_FILEPATH.FILE_KBN_CD="04") 
                         AND T_KOJI.HOMON_SBT="01"
                         AND T_KOJI.DEL_FLG=0';
                         $this->result = $this->dbConnect->query($sql);
@@ -373,8 +375,9 @@ class resources
                         T_KOJI_FILEPATH.FILEPATH_ID
                         FROM T_KOJI LEFT JOIN T_KOJI_FILEPATH ON T_KOJI.JYUCYU_ID=T_KOJI_FILEPATH.ID 
                         WHERE SYUYAKU_JYUCYU_ID= "' . $JYUCYU_ID . '"
+                        AND (T_KOJI_FILEPATH.FILE_KBN_CD="03" OR T_KOJI_FILEPATH.FILE_KBN_CD="04") 
                         AND T_KOJI.HOMON_SBT="02"
-                        AND T_KOJI.DEL_FLG=0';
+                        AND T_KOJI.DEL_FLG=0';                        
                         $this->result = $this->dbConnect->query($sql);
                         if ($this->result->num_rows > 0) {
                             // output data of each row                    
@@ -768,8 +771,8 @@ class resources
                             $data['KOJIGYOSYA_CD'] = $row['KOJIGYOSYA_CD'];
                             $data['KOJI_ST'] = $row['KOJI_ST'];
                             $data['HOJIN_FLG'] = $row['HOJIN_FLG'];
-                            $data['KOJIGYOSYA_NAME'] = $row['KOJIGYOSYA_NAME'];                           
-                            $data['STATUS'] = 'REPORTED';                           
+                            $data['KOJIGYOSYA_NAME'] = $row['KOJIGYOSYA_NAME'];
+                            $data['STATUS'] = 'REPORTED';
 
                             $resultSet['KOJI_DATA'][] = $data;
                         }
@@ -791,15 +794,15 @@ class resources
                         // output data of each row                    
                         while ($row = $this->result->fetch_assoc()) {
                             $data = array();
-                            $data['STATUS'] = $row['JYUCYU_ID'];                           
+                            $data['STATUS'] = $row['JYUCYU_ID'];
                             $data['TUIKA_SYOHIN_NAME'] = $row['TUIKA_SYOHIN_NAME'];
-                            $data['TUIKA_JISYA_CD'] = substr($row['TUIKA_JISYA_CD'] , -4);
+                            $data['TUIKA_JISYA_CD'] = substr($row['TUIKA_JISYA_CD'], -4);
                             $data['SURYO'] = $row['SURYO'];
                             $data['HANBAI_TANKA'] = $row['HANBAI_TANKA'];
-                            $data['KINGAK'] = $row['KINGAK'];                           
+                            $data['KINGAK'] = $row['KINGAK'];
                             $resultSet['TABLE_DATA'][] = $data;
                         }
-                    }                                    
+                    }
 
                     $sql = 'SELECT * FROM M_KOJI_KAKAKU WHERE DEL_FLG=0';
                     $this->result = $this->dbConnect->query($sql);
@@ -860,7 +863,7 @@ class resources
                 FILEPATH_ID,
                 FILE_KBN_CD,
                 ID FROM T_KOJI_FILEPATH
-                WHERE ID="'.$JYUCYU_ID.'" 
+                WHERE ID="' . $JYUCYU_ID . '" 
                 AND DEL_FLG=0';
                 $this->result = $this->dbConnect->query($sql);
                 if ($this->result->num_rows > 0) {
@@ -869,7 +872,6 @@ class resources
                         $resultSet['FILE'][] = $row;
                     }
                 }
-
             }
 
             if (!empty($resultSet)) {
@@ -949,7 +951,7 @@ class resources
 
                             foreach ($listJyucyuIdKoji as $value) {
                                 //Get HOJIN_FLG
-                                    $sqlGetHojinFlg = 'SELECT HOJIN_FLG, TENPO_CD 
+                                $sqlGetHojinFlg = 'SELECT HOJIN_FLG, TENPO_CD 
                                     FROM T_KOJI 
                                     WHERE JYUCYU_ID="' . $value['JYUCYU_ID'] . '" 
                                 ';
@@ -3787,58 +3789,72 @@ class resources
         if ($this->dbConnect == NULL) {
             $this->dbReference->sendResponse(503, '{"error_message":' . $this->dbReference->getStatusCodeMeeage(503) . '}');
         } else {
-            $sql = 'INSERT INTO `T_KOJI` 
-            (`JYUCYU_ID`, `SITAMI_YMD`, `KOJI_YMD`, 
-            `HOMON_TANT_CD1`, `HOMON_TANT_NAME1`, `HOMON_TANT_CD2`, `HOMON_TANT_NAME2`, `HOMON_TANT_CD3`, `HOMON_TANT_NAME3`, `HOMON_TANT_CD4`, `HOMON_TANT_NAME4`, 
-            `SETSAKI_NAME`, `SETSAKI_ADDRESS`, `KOJI_JININ`, `SITAMI_JININ`, `HOMON_SBT`, `KOJI_ST`, 
-            `KOJI_ITEM`, `SITAMI_KANSAN_POINT`, `KOJI_KANSAN_POINT`, 
-            `SITAMI_JIKAN`, `KOJI_JIKAN`, `KOJI_KEKKA`, `TENPO_CD`, `HOJIN_FLG`, `MALL_CD`, `KOJIGYOSYA_CD`, `TAG_KBN`, 
-            `SITAMIHOMONJIKAN`, `SITAMIHOMONJIKAN_END`, `KOJIHOMONJIKAN`, `KOJIHOMONJIKAN_END`, 
-            `KOJIIRAISYO_FILEPATH`, `SITAMIIRAISYO_FILEPATH`, 
-            `CANCEL_RIYU`, `SITAMIAPO_KBN`, `KOJIAPO_KBN`, `MTMORI_YMD`, `MEMO`, `COMMENT`, 
-            `READ_FLG`, `ATOBARAI`, `BIKO`, `SYUYAKU_JYUCYU_ID`, `REPORT_FLG`, `SITAMI_REPORT`, `ALL_DAY_FLG`, 
-            `CO_NAME`, `CO_POSTNO`, `CO_ADDRESS`, `KOJI_ITAKUHI`, `SKJ_RENKEI_YMD`, `KOJI_RENKEI_YMD`, `DEL_FLG`, 
-            `ADD_PGID`, `ADD_TANTCD`, `ADD_TANTNM`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_TANTNM`, `UPD_YMD`) 
-            VALUES 
-            ("0301416580", "2022-12-02", "2022-12-02", 
-            "00000", "AAAAA", "01010", "BBBBB", "02051", "CCCCC", "10018", "DDDDD", 
-            "EEEEE", "ADDRESS_TEST", "1", "1", "01", "01", 
-            "ITEM", "0.0", "0.0", 
-            "2", "2", "01", "01010", "0", "02", "00000", "05", 
-            "1000", "1300", "1000", "1500", 
-            "pdf/test.pdf", "pdf/test.pdf", 
-            NULL, "01", "01", "2022-12-02", "MEMO_ HELLO WORLD", "COMMENT_ HELLO WORLD", 
-            "1", NULL, NULL, NULL, NULL, NULL, NULL, 
-            "CO_NAME TEST", "6510084", "CO_ADDRESS TEST", "6000", "2022-12-02", "2022-12-02", "0", 
-            "KOJ1120F", "00000", "TEST", "2022-12-02 12:58:38", "KOJ1120F", "00000", "TEST", "2022-12-02 06:58:36.000000");';
+            for ($i = 3; $i <= 9; $i++) {
+                // $sql = 'INSERT INTO `T_KOJI` 
+                // (`JYUCYU_ID`, `SITAMI_YMD`, `KOJI_YMD`, 
+                // `HOMON_TANT_CD1`, `HOMON_TANT_NAME1`, `HOMON_TANT_CD2`, `HOMON_TANT_NAME2`, `HOMON_TANT_CD3`, `HOMON_TANT_NAME3`, `HOMON_TANT_CD4`, `HOMON_TANT_NAME4`, 
+                // `SETSAKI_NAME`, `SETSAKI_ADDRESS`, `KOJI_JININ`, `SITAMI_JININ`, `HOMON_SBT`, `KOJI_ST`, 
+                // `KOJI_ITEM`, `SITAMI_KANSAN_POINT`, `KOJI_KANSAN_POINT`, 
+                // `SITAMI_JIKAN`, `KOJI_JIKAN`, `KOJI_KEKKA`, `TENPO_CD`, `HOJIN_FLG`, `MALL_CD`, `KOJIGYOSYA_CD`, `TAG_KBN`, 
+                // `SITAMIHOMONJIKAN`, `SITAMIHOMONJIKAN_END`, `KOJIHOMONJIKAN`, `KOJIHOMONJIKAN_END`, 
+                // `KOJIIRAISYO_FILEPATH`, `SITAMIIRAISYO_FILEPATH`, 
+                // `CANCEL_RIYU`, `SITAMIAPO_KBN`, `KOJIAPO_KBN`, `MTMORI_YMD`, `MEMO`, `COMMENT`, 
+                // `READ_FLG`, `ATOBARAI`, `BIKO`, `SYUYAKU_JYUCYU_ID`, `REPORT_FLG`, `SITAMI_REPORT`, `ALL_DAY_FLG`, 
+                // `CO_NAME`, `CO_POSTNO`, `CO_ADDRESS`, `KOJI_ITAKUHI`, `SKJ_RENKEI_YMD`, `KOJI_RENKEI_YMD`, `DEL_FLG`, 
+                // `ADD_PGID`, `ADD_TANTCD`, `ADD_TANTNM`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_TANTNM`, `UPD_YMD`) 
+                // VALUES 
+                // ("030147348'.$i.'", "2022-12-05", "2022-12-05", 
+                // "00000", "AAAAA", "01010", "BBBBB", "02051", "CCCCC", "10018", "DDDDD", 
+                // "EEEEE", "ADDRESS_TEST", "1", "1", "01", "01", 
+                // "ITEM", "0.0", "0.0", 
+                // "2", "2", "01", "01010", "0", "02", "00000", "05", 
+                // "1000", "1300", "1000", "1500", 
+                // "pdf/test.pdf", "pdf/test.pdf", 
+                // NULL, "01", "01", "2022-12-05", "MEMO_ HELLO WORLD", "COMMENT_ HELLO WORLD", 
+                // "1", NULL, NULL, NULL, NULL, NULL, NULL, 
+                // "CO_NAME TEST", "6510084", "CO_ADDRESS TEST", "6000", "2022-12-05", "2022-12-05", "0", 
+                // "KOJ1120F", "00000", "TEST", "2022-12-05 12:58:38", "KOJ1120F", "00000", "TEST", "2022-12-05 06:58:36.000000");';
+
+                // $sql = 'INSERT INTO `T_KOJI_FILEPATH` 
+                // (`FILEPATH_ID`, `ID`, `FILEPATH`, `FILE_KBN_CD`, `DEL_FLG`, 
+                // `ADD_PGID`, `ADD_TANTCD`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_YMD`) 
+                // VALUES 
+                // ("170015466'.$i.'", "0301473482", "pdf/test'.$i.'.pdf", "03", "0", 
+                // "KOJ0060F", "00000", "2022-12-05 13:25:58", "KOJ0060F", "00000", "2022-12-05 07:25:57")';
+
+                // $sql = 'INSERT INTO `T_KOJIMSAI` 
+                // (`JYUCYU_ID`, `JYUCYUMSAI_ID`, `JYUCYUMSAI_ID_KIKAN`, `HINBAN`, `MAKER_CD`, 
+                // `CTGORY_CD`, `SURYO`, `HANBAI_TANKA`, `KINGAK`, `KISETU_HINBAN`, `KISETU_MAKER_CD`, `KENSETU_KEITAI`, 
+                // `BEF_SEKO_PHOTO_FILEPATH`, `AFT_SEKO_PHOTO_FILEPATH`, `OTHER_PHOTO_FOLDERPATH`, 
+                // `TUIKA_JISYA_CD`, `TUIKA_SYOHIN_NAME`, `KOJIJITUIKA_FLG`, `DEL_FLG`, `RENKEI_YMD`, 
+                // `ADD_PGID`, `ADD_TANTCD`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_YMD`) 
+                // VALUES 
+                // ("030147348'.$i.'", "000000001'.$i.'", "000000001'.$i.'", "KOJ-0000", "00000",
+                //  "000", "1", "160'.$i.'", "160'.$i.'", NULL, NULL, NULL, 
+                //  "img/pic.png", "img/pic2.png", NULL, 
+                //  "KOJ-00000", "TUIKA_SYOHIN_NAME", "1", "0", NULL, 
+                //  NULL, "00000", "2022-12-02 13:28:39", NULL, "00000", "2022-12-02 07:28:38");';
+
+                // $sql = 'INSERT INTO `T_TIRASI` 
+                // (`TANT_CD`, `YMD`, `KOJI_TIRASISU`, `RENKEI_YMD`, `DEL_FLG`, 
+                // `ADD_PGID`, `ADD_TANTCD`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_YMD`) 
+                // VALUES 
+                // ("00000", "2022-12-0'.$i.'", "999", NULL, "0", 
+                // "KOJ0990B", "00000", "2022-12-02 13:41:28", "KOJ0990B", "00000", "2022-12-02 07:41:28")';
+                
+                $this->result = $this->dbConnect->query($sql);
+            }
         }
+    }
 
-
-        $sql = 'INSERT INTO `T_KOJI_FILEPATH` 
-        (`FILEPATH_ID`, `ID`, `FILEPATH`, `FILE_KBN_CD`, `DEL_FLG`, 
-        `ADD_PGID`, `ADD_TANTCD`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_YMD`) 
-        VALUES 
-        ("1700154648", "0301416580", "pdf/test.pdf", "03", "0", 
-        "KOJ0060F", "00000", "2022-12-02 13:25:58", "KOJ0060F", "00000", "2022-12-02 07:25:57.000000")';
-
-        $sql = 'INSERT INTO `T_KOJIMSAI` 
-        (`JYUCYU_ID`, `JYUCYUMSAI_ID`, `JYUCYUMSAI_ID_KIKAN`, `HINBAN`, `MAKER_CD`, 
-        `CTGORY_CD`, `SURYO`, `HANBAI_TANKA`, `KINGAK`, `KISETU_HINBAN`, `KISETU_MAKER_CD`, `KENSETU_KEITAI`, 
-        `BEF_SEKO_PHOTO_FILEPATH`, `AFT_SEKO_PHOTO_FILEPATH`, `OTHER_PHOTO_FOLDERPATH`, 
-        `TUIKA_JISYA_CD`, `TUIKA_SYOHIN_NAME`, `KOJIJITUIKA_FLG`, `DEL_FLG`, `RENKEI_YMD`, 
-        `ADD_PGID`, `ADD_TANTCD`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_YMD`) 
-        VALUES 
-        ("0301416580", "0000000000", "0000000000", "KOJ-0000", "00000",
-         "000", "1", "1600", "1600", NULL, NULL, NULL, 
-         "img/pic.png", "img/pic.png", NULL, 
-         "KOJ-00000", "TUIKA_SYOHIN_NAME", "1", "0", NULL, 
-         NULL, "00000", "2022-12-02 13:28:39", NULL, "00000", "2022-12-02 07:28:38.000000");';
-
-        $sql = 'INSERT INTO `T_TIRASI` 
-        (`TANT_CD`, `YMD`, `KOJI_TIRASISU`, `RENKEI_YMD`, `DEL_FLG`, 
-        `ADD_PGID`, `ADD_TANTCD`, `ADD_YMD`, `UPD_PGID`, `UPD_TANTCD`, `UPD_YMD`) 
-        VALUES 
-        ("00000", "2022-12-02", "999", NULL, "0", 
-        "KOJ0990B", "00000", "2022-12-02 13:41:28", "KOJ0990B", "00000", "2022-12-02 07:41:28.000000");';
+    function update_syuyaku_jyucyu_id() {
+        $this->dbReference = new systemConfig();
+        $this->dbConnect = $this->dbReference->connectDB();
+        if ($this->dbConnect == NULL) {
+            $this->dbReference->sendResponse(503, '{"error_message":' . $this->dbReference->getStatusCodeMeeage(503) . '}');
+        } else {
+            $sql = 'UPDATE T_KOJI SET SYUYAKU_JYUCYU_ID=NULL WHERE SYUYAKU_JYUCYU_ID IS NOT NULL OR SYUYAKU_JYUCYU_ID=""';           
+            $this->result = $this->dbConnect->query($sql);
+        }
     }
 }
---
+++ temp_update.php
@@ -0,0 +1,8 @@
+<?php
+
+include('resources.php');
+// $insert = new resources();
+// $insert->insert();
+
+$update_syuyaku_jyucyu_id = new resources();
+$update_syuyaku_jyucyu_id->update_syuyaku_jyucyu_id();
